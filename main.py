b42d4d045b401959784784731254e81fa70d572a
LizWise@github.com <elizabethmmudry@gmail.com>
2026-01-21 16:44:02 +0900

    Tested Browse API using item_summary resource

--- main.py
+++ main.py
@@ -6,31 +6,59 @@ from dotenv import load_dotenv
 
 load_dotenv("sandbox.env")
 
+def get_token():
 
-key = os.getenv("EBAY_CLIENT_ID")
-secret = os.getenv("EBAY_CLIENT_SECRET")
+    key = os.getenv("EBAY_CLIENT_ID")
+    secret = os.getenv("EBAY_CLIENT_SECRET")
 
-token_url = "https://api.sandbox.ebay.com/identity/v1/oauth2/token"
+    token_url = "https://api.sandbox.ebay.com/identity/v1/oauth2/token"
 
-scopes = "https://api.ebay.com/oauth/api_scope" 
+    scopes = "https://api.ebay.com/oauth/api_scope" 
 
-creds = f"{key}:{secret}"
-encoded_creds = base64.b64encode(creds.encode()).decode()
+    creds = f"{key}:{secret}"
+    encoded_creds = base64.b64encode(creds.encode()).decode()
 
-headers = {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Authorization": f"Basic {encoded_creds}"
-}
+    headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": f"Basic {encoded_creds}"
+        }
 
-data = {
-    "grant_type": "client_credentials",
-    "scope": scopes
-}
+    data = {
+            "grant_type": "client_credentials",
+            "scope": scopes
+        }
 
-response = requests.post(token_url, headers=headers, data=data)
-token_data = response.json()
+    response = requests.post(token_url, headers=headers, data=data)
+    token_data = response.json()
 
-access_token = token_data["access_token"]
+    access_token = token_data["access_token"]
+    return access_token
+
+x = get_token()
+
+def get_items():
+    uri = "https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search"
+
+    headers = {
+        "Authorization": f"Bearer {x}"
+    }
+
+    params = {
+        "q" : "apples",
+        "limit" : 3,     
+    }
+
+    response = requests.get(uri, headers=headers, params=params)
+    return response.text
+
+y = get_items()
+print(f"Here are some apples: {y}")
+
+
+
+
+
 
     
-print("Status Code: ", response.status_code)
+
+
